80c7d84d1cecf055237f2704f3456234c156046f
Vishwanatha Subbanna <vishwabmc@users.noreply.github.com>
2022-03-21 23:58:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = "~> 1.39.0"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+  }
+}
+
 # Create an arbitrary local resource
 data "template_file" "test" {
   template = "Hello ${var.name}!"
